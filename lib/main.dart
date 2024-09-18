f27cfedb160341d8573aec5e414533df9508341a
Amal168 <amalsubala2024@gmail.com>
2024-09-17 21:42:01 -0700

    17-09-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterproject/chessboard.dart';
+import 'package:flutterproject/circle.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: chessboard(),
+      home: Circle(),
     );
   }
 }
